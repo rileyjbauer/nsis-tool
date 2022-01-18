4d34a38df36a116ed3441185f300e0769ce24bb9
rileyjbauer <rbauer1212@gmail.com>
2022-01-18 11:16:02 -0500

    Top nav now collapses into menu button for mobile/small screens

--- src/components/basicPage/BasicPage.tsx
+++ src/components/basicPage/BasicPage.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { ArrowNavBar } from '../arrowNavBar/ArrowNavBar';
+import MenuIcon from '@mui/icons-material/Menu';
 import './BasicPage.css';
+import { useState } from 'react';
 
 interface BasicPageProps {
   title?: string;
@@ -10,20 +12,26 @@ interface BasicPageProps {
 }
 
 export function BasicPage(props: BasicPageProps) {
+  const [menuExpanded, setMenuExpanded] = useState(false);
   return (
     <div>
       <div className='header-bar'>
-        <Link to='/' style={{ textDecoration: 'none' }}>
-          <h1 className='header-bar-title'>NSIS Tool</h1>
-        </Link>
+        <span className='title-bar'>
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <h1 className='header-bar-title'>NSIS Tool</h1>
+          </Link>
+          <div className='menu-button'>
+            <MenuIcon sx={{ fontSize: 50 }} onClick={() => setMenuExpanded(!menuExpanded)} />
+          </div>
+        </span>
         <hr className='header-hr' />
-        <div className='nav-bar'>
+        <div className={`nav-bar ${menuExpanded ? 'menu-expanded' : ''}`}>
           <Link to='/mainNav'>Start Over</Link>
           <Link to='/interventionsList'>All Nutrition Interventions</Link>
           <Link to='/genderIntegrationsList'>All Gender Integrations</Link>
         </div>
       </div>
-      <div className="content">
+      <div className="content" onClick={() => setMenuExpanded(false)}>
         {props.title && <h1>{props.title}</h1>}
         {props.content}
         <ArrowNavBar back={props.navBackward} forward={props.navForward} />
