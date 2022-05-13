cb0c86c16f2268243261f9ac5b8abf61f280b6ad
rileyjbauer <rbauer1212@gmail.com>
2022-05-13 17:06:18 -0500

    Adds search and filtering to the intervention list page. The same will need to be done for the gender integration list page, which probably means the two should be combined

--- src/pages/lists/InterventionsList.tsx
+++ src/pages/lists/InterventionsList.tsx
@@ -1,12 +1,79 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Select, { OnChangeValue } from 'react-select';
 import { BasicPage } from '../../components/basicPage/BasicPage';
 import Interventions from '../../data/interventions.json';
 import './List.css';
 
+type Option = {
+  value: string;
+  label: string;
+}
+
+const options: Option[] = [
+  { value: 'Availability', label: 'Availability' },
+  { value: 'Accessibility', label: 'Accessibility' },
+  { value: 'Affordability', label: 'Affordability' },
+  { value: 'Convenience', label: 'Convenience' },
+  { value: 'Desirability', label: 'Desirability' },
+  { value: 'Price', label: 'Price' },
+  { value: 'Women\'s Empowerment', label: 'Women\'s empowerment' }
+]
+
 export function InterventionsList() {
-  const content =
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filterTerms, setFilterTerms] = useState([] as Option[]);
+
+  function handleFilterChange(options: OnChangeValue<Option, true>): void {
+    setFilterTerms(options as Option[]);
+  }
+
+  function reset(): void {
+    setFilterTerms([]);
+    setSearchTerm('');
+  }
+
+  let filteredInterventions = Interventions.interventions;
+  if (filterTerms.length > 0) {
+    filteredInterventions = Interventions.interventions.filter(
+      (intervention) => {
+        for (let i = 0; i < filterTerms.length; i++) {
+          console.log(filterTerms[i].value);
+          if (!intervention.foodEnvironmentDomainsAffected.includes(filterTerms[i].value)) {
+            return false;
+          }
+        }
+        return true;
+      }
+    );
+  }
+  filteredInterventions = filteredInterventions.filter(
+    (intervention) => intervention.title.includes(searchTerm));
+
+  const content = (
     <div className='list-container'>
-      {Interventions.interventions.map((intervention, i) => {
+      <div className='list-searchbar'>
+        <label className='InputForm-label'>
+          <span className='InputForm-span'>Filter by food domain:</span>
+          <Select
+            isMulti
+            name='Food Domains'
+            options={options}
+            value={filterTerms}
+            onChange={handleFilterChange}
+          />
+        </label>
+
+        <label className='InputForm-label'>
+          <span className='InputForm-span'>Search:</span>
+          <input name='searchField' className='InputForm-text-field' type='text' onChange={(event) => setSearchTerm(event.currentTarget.value)} value={searchTerm} />
+        </label>
+
+        <button className='list-searchbar-reset-button' onClick={reset}>
+          Reset
+        </button>
+      </div>
+      {filteredInterventions.map((intervention, i) => {
         if (intervention.title) {
           return (
             <div key={i}>
@@ -23,6 +90,8 @@ export function InterventionsList() {
         return <div key={i} />;
       })}
     </div>
+  );
+
   return (
     <BasicPage
       title="All Nutrition Interventions"
